fe36ebcbc554bd57ffb28e4204692c24d2babbe7
Damodara Martin Tränkner <Martin.Traenkner@westerwald.yoga-vidya.de>
2026-08-31 00:54:23 +0200

    Aktualize Revision.

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,5 +1,5 @@
 #ifndef __REVISION_DATA_H__
 #define __REVISION_DATA_H__
- #define REVISION_HASH                      "2644fbae6947a299fb2a"
- #define REVISION_DATE                      "2026-08-30 22:25:53 +0200"
+ #define REVISION_HASH                      "c348c2852ba3546f2969"
+ #define REVISION_DATE                      "2026-08-31 00:07:22 +0200"
 #endif // __REVISION_DATA_H__
