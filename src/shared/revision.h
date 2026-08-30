1047649ed806fda4e9bb900c40332888ca7e387c
Damodara Martin Tränkner <Martin.Traenkner@westerwald.yoga-vidya.de>
2026-08-30 22:44:39 +0200

    Fixing Core Based Build Patch that only affetc wen the builder uses USE_PCH=OFF

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,5 +1,5 @@
 #ifndef __REVISION_DATA_H__
 #define __REVISION_DATA_H__
- #define REVISION_HASH                      "fb514c7447a7cd8d95ba"
- #define REVISION_DATE                      "2026-08-26 19:34:22 +0200"
+ #define REVISION_HASH                      "2644fbae6947a299fb2a"
+ #define REVISION_DATE                      "2026-08-30 22:25:53 +0200"
 #endif // __REVISION_DATA_H__
