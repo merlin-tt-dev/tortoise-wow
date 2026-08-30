d3d562e4f5aab66d582fdf68270443732b68eab4
Damodara Martin Tränkner <Martin.Traenkner@westerwald.yoga-vidya.de>
2026-08-30 14:27:19 +0200

    Build/Compile Audit: Native Checkmail Master Player

--- modules/mod-playerbots/src/playerbot/strategy/actions/CheckMailAction.cpp
+++ modules/mod-playerbots/src/playerbot/strategy/actions/CheckMailAction.cpp
@@ -2,6 +2,7 @@
 #include "playerbot/playerbot.h"
 #include "CheckMailAction.h"
 #include "Mail/Mail.h"
+#include "MapNodes/MasterPlayer.h"
 
 #include "playerbot/PlayerbotAIConfig.h"
 using namespace ai;
@@ -9,14 +10,18 @@ using namespace ai;
 bool CheckMailAction::Execute(Event& event)
 {
     WorldPacket p;
-    bot->GetSession()->HandleQueryNextMailTime(p);   
+    bot->GetSession()->HandleQueryNextMailTime(p);
+
+    MasterPlayer* masterPlayer = bot->GetSession()->GetMasterPlayer();
+    if (!masterPlayer)
+        return false;
 
     std::list<uint32> ids;
 
     PlayerMails mails;
 
     //Fetch mails first and then loop over them to prevent needing to check mails sent to self.
-    for (PlayerMails::iterator i = bot->GetMailBegin(); i != bot->GetMailEnd(); ++i)
+    for (PlayerMails::iterator i = masterPlayer->GetMailBegin(); i != masterPlayer->GetMailEnd(); ++i)
     {
         mails.push_back(*i);
     }
@@ -42,10 +47,10 @@ bool CheckMailAction::Execute(Event& event)
     for (std::list<uint32>::iterator i = ids.begin(); i != ids.end(); ++i)
     {
         uint32 id = *i;
-        bot->SendMailResult(id, MAIL_DELETED, MAIL_OK);
+        bot->GetSession()->SendMailResult(id, MAIL_DELETED, MAIL_OK);
         CharacterDatabase.PExecute("DELETE FROM mail WHERE id = '%u'", id);
         CharacterDatabase.PExecute("DELETE FROM mail_items WHERE mail_id = '%u'", id);
-        bot->RemoveMail(id);
+        masterPlayer->RemoveMail(id);
     }
 
     return true;
@@ -53,7 +58,8 @@ bool CheckMailAction::Execute(Event& event)
 
 bool CheckMailAction::isUseful()
 {
-    if (ai->GetMaster() || !bot->GetMailSize() || bot->InBattleGround())
+    MasterPlayer* masterPlayer = bot->GetSession()->GetMasterPlayer();
+    if (ai->GetMaster() || !masterPlayer || !masterPlayer->GetMailSize() || bot->InBattleGround())
         return false;
 
     return true;
@@ -72,10 +78,14 @@ void CheckMailAction::ProcessMail(Mail* mail, Player* owner)
         
     if (mail->messageType != MAIL_NORMAL || mail->stationery == MAIL_STATIONERY_AUCTION)
         return;
-        
+
+    MasterPlayer* masterPlayer = bot->GetSession()->GetMasterPlayer();
+    if (!masterPlayer)
+        return;
+
     for (MailItemInfoVec::iterator i = mail->items.begin(); i != mail->items.end(); ++i)
     {
-        Item *item = bot->GetMItem(i->item_guid);
+        Item *item = masterPlayer->GetMItem(i->item_guid);
         if (!item)
             continue;
 
@@ -89,7 +99,7 @@ void CheckMailAction::ProcessMail(Mail* mail, Player* owner)
 
         MailDraft draft("Item(s) you've sent me", body.str());
         draft.AddItem(item);
-        bot->RemoveMItem(i->item_guid);
+        masterPlayer->RemoveMItem(i->item_guid);
         draft.SendMailTo(MailReceiver(owner), MailSender(bot));
         return;
     }
