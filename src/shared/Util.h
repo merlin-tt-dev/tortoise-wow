15c050039280762bbcf7e64e85ff1927286a1289
damodara <damodara@posteo.com>
2026-09-10 01:54:51 +0200

    Core Optimization: Single Allocation depracted format

--- src/shared/Util.h
+++ src/shared/Util.h
@@ -129,9 +129,10 @@ std::string string_format_depr(const std::string& format, Args ... args)
     int size_s = std::snprintf(nullptr, 0, format.c_str(), args ...) + 1; // Extra space for '\0'
     if (size_s <= 0) { throw std::runtime_error("Error during formatting."); }
     auto size = static_cast<size_t>(size_s);
-    std::unique_ptr<char[]> buf(new char[size]);
-    std::snprintf(buf.get(), size, format.c_str(), args ...);
-    return std::string(buf.get(), buf.get() + size - 1); // We don't want the '\0' inside
+    std::string result(size, '\0');
+    std::snprintf(result.data(), size, format.c_str(), args ...);
+    result.resize(size - 1); // We don't want the '\0' inside
+    return result;
 }
 
 
